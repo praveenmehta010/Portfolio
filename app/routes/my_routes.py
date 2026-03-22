b30c18c54fc29969ed2d8206e45184007543da76
Praveen Mehta <praveen.mehta010.com>
2026-03-22 15:08:24 +0530

    change porfile img

--- app/routes/my_routes.py
+++ app/routes/my_routes.py
@@ -1,12 +1,18 @@
+import os
+
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from app.models.contact import Contact
 from app.config.mongo_db_connection import contacts, projects
 
+from dotenv import load_dotenv
+
+load_dotenv()
 routes = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
+PROFILE_IMAGE_URL = os.getenv("PROFILE_IMAGE_URL")
 def fetch_projects():
     docs = list(projects.find({}))
     # print(docs)
@@ -19,7 +25,7 @@ async def read_item(request: Request):
     return templates.TemplateResponse(
         request=request, name="index.html",
         # Pass the data to the template in a context dictionary
-        context={"projects": projects}
+        context={"projects": projects, "PROFILE_IMAGE_URL":PROFILE_IMAGE_URL}
     )
     
     
